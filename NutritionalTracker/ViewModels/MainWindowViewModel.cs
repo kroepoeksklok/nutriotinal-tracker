87f987d8f98e2640b666614d4346ec1a621ae7fc
Robbert <robbert.ouwehand@gmail.com>
2019-10-16 13:29:27 +0200

    Introduce Task that refreshes data constantly
    It was getting rather annoying to restart the app every time data was
    added to the database.
    
    This change addresses that by adding a background task that retrieves
    the newest data every ten seconds. It also preserves the selection of
    the user for a better user experience.
    
    This change has no side effects.
    
    Not exactly keeping track of issues in Github, sorry.

--- NutritionalTracker/ViewModels/MainWindowViewModel.cs
+++ NutritionalTracker/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
 using NutritionalTracker.Commands;
 using NutritionalTracker.Database;
 using NutritionalTracker.Mappers;
@@ -10,13 +13,11 @@ using NutritionalTracker.Models;
 using NutritionalTracker.Queries;
 using ICommand = System.Windows.Input.ICommand;
 
-namespace NutritionalTracker.ViewModels
-{
-    public sealed class MainWindowViewModel : INotifyPropertyChanged
-    {
-        private List<Models.Recipe> _recipes;
-        private List<Models.Product> _products;
-        private List<Models.Meal> _meals;
+namespace NutritionalTracker.ViewModels {
+    public sealed class MainWindowViewModel : INotifyPropertyChanged {
+        private ObservableCollection<Models.Recipe> _recipes;
+        private ObservableCollection<Models.Product> _products;
+        private ObservableCollection<Models.Meal> _meals;
         private Models.Recipe _selectedRecipe;
         private Models.Product _selectedProduct;
         private Models.Meal _selectedMeal;
@@ -33,13 +34,21 @@ namespace NutritionalTracker.ViewModels
 
         private IReadOnlyList<FoodLog> _logEntries;
 
-        public MainWindowViewModel(IQueryProcessor queryProcessor, ICommandProcessor commandProcessor, IMapperProcessor mapperProcessor)
-        {
+        public MainWindowViewModel(IQueryProcessor queryProcessor, ICommandProcessor commandProcessor, IMapperProcessor mapperProcessor) {
             _mapperProcessor = mapperProcessor ?? throw new ArgumentNullException(nameof(mapperProcessor));
             _queryProcessor = queryProcessor ?? throw new ArgumentNullException(nameof(queryProcessor));
             _commandProcessor = commandProcessor ?? throw new ArgumentNullException(nameof(commandProcessor));
 
-            LoadData();
+            var data = GetData();
+
+            Recipes = new ObservableCollection<Models.Recipe>(data.Item1);
+            SelectedRecipe = Recipes.First();
+
+            Products = new ObservableCollection<Models.Product>(data.Item2);
+            SelectedProduct = Products.First();
+
+            Meals = new ObservableCollection<Models.Meal>(data.Item3);
+            SelectedMeal = Meals.First();
 
             SelectedDate = DateTime.Now.Date;
             Unit = "gr / ml";
@@ -47,6 +56,29 @@ namespace NutritionalTracker.ViewModels
             AddProductToDiary = new RelayCommand(AddProductToDiaryHandler, o => SelectedProduct != null && _amountOfProductConsumed > 0 && SelectedMeal != null);
             AddRecipeToDiary = new RelayCommand(AddRecipeToDiaryHandler, o => SelectedRecipe != null && _servingsEaten > 0 && SelectedMeal != null);
             DeleteFoodLog = new RelayCommand(DeleteFoodLogHandler);
+
+            Task.Run(() => {
+                while (true) {
+                    Thread.Sleep(10000);
+                    var newData = GetData();
+
+                    var currentlySelectedProductId = SelectedProduct.ProductId;
+                    var currentlySelectedMealId = SelectedMeal.MealId;
+                    var currentlySelectedRecipeId = SelectedRecipe.RecipeId;
+
+                    SelectedMeal = null;
+                    SelectedProduct = null;
+                    SelectedRecipe = null;
+
+                    Recipes = new ObservableCollection<Models.Recipe>(newData.Item1);
+                    Products = new ObservableCollection<Models.Product>(newData.Item2);
+                    Meals = new ObservableCollection<Models.Meal>(newData.Item3);
+
+                    SelectedRecipe = Recipes.FirstOrDefault(r => r.RecipeId == currentlySelectedRecipeId);
+                    SelectedProduct = Products.FirstOrDefault(p => p.ProductId == currentlySelectedProductId);
+                    SelectedMeal = Meals.FirstOrDefault(m => m.MealId == currentlySelectedMealId);
+                }
+            });
         }
 
         public ICommand AddRecipeToDiary { get; set; }
@@ -54,134 +86,110 @@ namespace NutritionalTracker.ViewModels
         public ICommand DeleteFoodLog { get; set; }
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public string Unit
-        {
+        public string Unit {
             get => _unit;
-            set
-            {
+            set {
                 _unit = value;
                 OnPropertyChanged(nameof(Unit));
             }
         }
 
-        public List<Models.Recipe> Recipes
-        {
+        public ObservableCollection<Models.Recipe> Recipes {
             get => _recipes;
-            set
-            {
+            set {
                 _recipes = value;
                 OnPropertyChanged(nameof(Recipes));
             }
         }
 
-        public Models.Recipe SelectedRecipe
-        {
+        public Models.Recipe SelectedRecipe {
             get => _selectedRecipe;
-            set
-            {
+            set {
                 _selectedRecipe = value;
                 OnPropertyChanged(nameof(SelectedRecipe));
             }
         }
 
-        public double ServingsEaten
-        {
+        public double ServingsEaten {
             get => _servingsEaten;
-            set
-            {
+            set {
                 _servingsEaten = value;
                 OnPropertyChanged(nameof(ServingsEaten));
             }
         }
 
 
-        public List<Models.Product> Products
-        {
+        public ObservableCollection<Models.Product> Products {
             get => _products;
-            set
-            {
+            set {
                 _products = value;
                 OnPropertyChanged(nameof(Products));
             }
         }
 
-        public Models.Product SelectedProduct
-        {
+        public Models.Product SelectedProduct {
             get => _selectedProduct;
-            set
-            {
+            set {
                 _selectedProduct = value;
                 OnPropertyChanged(nameof(SelectedProduct));
-                Unit = value.Unit;
+                if (value != null) {
+                    Unit = value.Unit;
+                }
             }
         }
 
-        public int AmountOfProductConsumed
-        {
+        public int AmountOfProductConsumed {
             get => _amountOfProductConsumed;
-            set
-            {
+            set {
                 _amountOfProductConsumed = value;
                 OnPropertyChanged(nameof(AmountOfProductConsumed));
             }
         }
 
 
-        public List<Models.Meal> Meals
-        {
+        public ObservableCollection<Models.Meal> Meals {
             get => _meals;
-            set
-            {
+            set {
                 _meals = value;
                 OnPropertyChanged(nameof(Meals));
             }
         }
 
-        public Models.Meal SelectedMeal
-        {
+        public Models.Meal SelectedMeal {
             get => _selectedMeal;
-            set
-            {
+            set {
                 _selectedMeal = value;
                 OnPropertyChanged(nameof(SelectedMeal));
             }
         }
 
-        public DateTime SelectedDate
-        {
+        public DateTime SelectedDate {
             get => _selectedDate;
-            set
-            {
+            set {
                 _selectedDate = value;
                 OnPropertyChanged(nameof(SelectedDate));
                 LoadDailyData();
             }
         }
 
-        public DailyLog DailyLog
-        {
+        public DailyLog DailyLog {
             get => _dailyLog;
-            set
-            {
+            set {
                 _dailyLog = value;
                 OnPropertyChanged(nameof(DailyLog));
             }
         }
 
-        public Models.Statistics Statistics
-        {
+        public Models.Statistics Statistics {
             get => _statistics;
-            set
-            {
+            set {
                 _statistics = value;
                 OnPropertyChanged(nameof(Statistics));
             }
         }
 
-        private void AddProductToDiaryHandler(object parameter)
-        {
-            _commandProcessor.Process(new AddProductToDiaryCommand
-            {
+        private void AddProductToDiaryHandler(object parameter) {
+            _commandProcessor.Process(new AddProductToDiaryCommand {
                 AmountConsumed = AmountOfProductConsumed,
                 ProductId = SelectedProduct.ProductId,
                 MealId = SelectedMeal.MealId,
@@ -191,10 +199,8 @@ namespace NutritionalTracker.ViewModels
             LoadDailyData();
         }
 
-        private void AddRecipeToDiaryHandler(object parameter)
-        {
-            _commandProcessor.Process(new AddRecipeToDiaryCommand
-            {
+        private void AddRecipeToDiaryHandler(object parameter) {
+            _commandProcessor.Process(new AddRecipeToDiaryCommand {
                 NumberOfServingsConsumed = ServingsEaten,
                 RecipeId = SelectedRecipe.RecipeId,
                 MealId = SelectedMeal.MealId,
@@ -204,73 +210,64 @@ namespace NutritionalTracker.ViewModels
             LoadDailyData();
         }
 
-        private void DeleteFoodLogHandler(object parameter)
-        {
-            var logentry = (LogEntry) parameter;
-            _commandProcessor.Process(new DeleteFoodLogCommand
-            {
+        private void DeleteFoodLogHandler(object parameter) {
+            var logentry = (LogEntry)parameter;
+            _commandProcessor.Process(new DeleteFoodLogCommand {
                 FoodLogId = logentry.FoodLogId
             });
 
             LoadDailyData();
         }
-        
-        private void LoadData()
-        {
+
+        private Tuple<List<Models.Recipe>, List<Models.Product>, List<Models.Meal>> GetData() {
             var recipesQuery = new GetAllRecipesQuery();
             var dbRecipes = _queryProcessor.Process(recipesQuery);
-            Recipes = new List<Models.Recipe>(dbRecipes.Select(_mapperProcessor.Map<Database.Recipe, Models.Recipe>));
 
             var productsQuery = new GetAllProductsQuery();
             var dbProducts = _queryProcessor.Process(productsQuery);
-            Products = new List<Models.Product>(dbProducts.Select(_mapperProcessor.Map<Database.Product, Models.Product>));
 
             var mealsQuery = new GetAllMealsQuery();
             var dbMeals = _queryProcessor.Process(mealsQuery);
-            Meals = new List<Models.Meal>(dbMeals.Select(_mapperProcessor.Map<Database.Meal, Models.Meal>));
+
+            return Tuple.Create(
+                new List<Models.Recipe>(dbRecipes.Select(_mapperProcessor.Map<Database.Recipe, Models.Recipe>)),
+                new List<Models.Product>(dbProducts.Select(_mapperProcessor.Map<Database.Product, Models.Product>)),
+                new List<Models.Meal>(dbMeals.Select(_mapperProcessor.Map<Database.Meal, Models.Meal>)));
         }
 
-        private void LoadDailyData()
-        {
+        private void LoadDailyData() {
             LoadLogEntries();
             LoadFoodLog();
             LoadStatistics();
         }
-        
 
-        private void LoadLogEntries()
-        {
-            var getFoodLogQuery = new GetFoodLogQuery
-            {
+
+        private void LoadLogEntries() {
+            var getFoodLogQuery = new GetFoodLogQuery {
                 Date = SelectedDate
             };
             _logEntries = _queryProcessor.Process(getFoodLogQuery);
         }
 
-        private void LoadFoodLog()
-        {
+        private void LoadFoodLog() {
             var dailyLog = new DailyLog(_mapperProcessor, Meals);
-            foreach (var foodLog in _logEntries)
-            {
+            foreach (var foodLog in _logEntries) {
                 dailyLog.AddLogEntry(foodLog);
             }
 
             DailyLog = dailyLog;
         }
 
-        private void LoadStatistics()
-        {
+        private void LoadStatistics() {
             var statistics = new Models.Statistics();
-            foreach (var foodLog in _logEntries)
-            {
+            foreach (var foodLog in _logEntries) {
                 statistics.AddLogEntry(foodLog);
             }
 
             Statistics = statistics;
         }
 
-        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null) {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
