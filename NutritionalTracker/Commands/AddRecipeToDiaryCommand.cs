b2121d7b712c9fb5321ef5d5dbe1cc7d8a8021f8
Robbert <robbert.ouwehand@gmail.com>
2020-05-31 08:27:29 +0200

    Add missing brace

--- NutritionalTracker/Commands/AddRecipeToDiaryCommand.cs
+++ NutritionalTracker/Commands/AddRecipeToDiaryCommand.cs
@@ -2,9 +2,11 @@
 
 namespace NutritionalTracker.Commands
 {
-    public sealed class AddRecipeToDiaryCommand : ICommand {
+    public sealed class AddRecipeToDiaryCommand : ICommand
+    {
         public int RecipeId { get; set; }
         public double NumberOfServingsConsumed { get; set; }
         public byte MealId { get; set; }
         public DateTime ConsumedDate { get; set; }
     }
+}
