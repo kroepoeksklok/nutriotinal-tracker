d58b9b99c51f04c9210fa150182955cf3e0bea76
Robbert <robbert.ouwehand@gmail.com>
2020-05-31 08:24:29 +0200

    Add command handler to copy food logs

---
+++ NutritionalTracker/Commands/CopyToDiaryCommandHandler.cs
@@ -0,0 +1,27 @@
+﻿using NutritionalTracker.Database;
+using System.Data.Entity;
+using System.Linq;
+
+namespace NutritionalTracker.Commands
+{
+    public sealed class CopyToDiaryCommandHandler : ICommandHandler<CopyToDiaryCommand>
+    {
+        private readonly INutrionalModel _context;
+
+        public CopyToDiaryCommandHandler(INutrionalModel context) {
+            _context = context;
+        }
+
+        public void Handle(CopyToDiaryCommand command) {
+            var foodlogs = _context.FoodLogs
+                .Include(f => f.Product)
+                .Where(f => f.Date == command.DateToCopy.Date && f.MealId == command.MealId);
+
+            foreach(var foodlog in foodlogs) {
+                DiaryHelper.CopyFoodlog(_context, foodlog, command.MealId, command.DateToCopyTo);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
