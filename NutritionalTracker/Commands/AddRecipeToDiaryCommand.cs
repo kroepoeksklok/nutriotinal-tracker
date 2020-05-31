c7785f9c7a47f2840e26761dad9a79c78f0fbf60
Robbert <robbert.ouwehand@gmail.com>
2020-05-31 08:14:12 +0200

    Add command to copy a meal

--- NutritionalTracker/Commands/AddRecipeToDiaryCommand.cs
+++ NutritionalTracker/Commands/AddRecipeToDiaryCommand.cs
@@ -1,10 +1,10 @@
 ﻿using System;
 
-namespace NutritionalTracker.Commands {
+namespace NutritionalTracker.Commands
+{
     public sealed class AddRecipeToDiaryCommand : ICommand {
         public int RecipeId { get; set; }
         public double NumberOfServingsConsumed { get; set; }
         public byte MealId { get; set; }
         public DateTime ConsumedDate { get; set; }
     }
-}
---
+++ NutritionalTracker/Commands/CopyToDiaryCommand.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace NutritionalTracker.Commands
+{
+    public sealed class CopyToDiaryCommand : ICommand
+    {
+        public byte MealId { get; set; }
+        public DateTime DateToCopy { get; set; }
+    }
+}
