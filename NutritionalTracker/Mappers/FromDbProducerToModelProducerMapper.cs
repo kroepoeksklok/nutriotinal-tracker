aa67e0516decaf7c603803dc3c949455a84afe88
Robbert <robbert.ouwehand@gmail.com>
2021-05-08 14:20:01 +0200

    Add ProducerListItem -> Models.Producer mapper

---
+++ NutritionalTracker/Mappers/FromDbProducerToModelProducerMapper.cs
@@ -0,0 +1,11 @@
+﻿namespace NutritionalTracker.Mappers {
+    public sealed class FromDbProducerToModelProducerMapper : IMapper<Database.ProducerListItem, Models.Producer>     {
+        public Models.Producer Map(Database.ProducerListItem producer) {
+            return new Models.Producer {
+                ProducerId = producer.ProducerId,
+                Name = producer.Name,
+                CanBeDeleted = producer.CanBeDeleted
+            };
+        }
+    }
+}
