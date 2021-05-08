bf3a92800eb500e7c19d9247f38356578a9f3c30
Robbert <robbert.ouwehand@gmail.com>
2021-05-08 14:04:08 +0200

    Add handler to get all producers

---
+++ NutritionalTracker/Queries/GetAllProducersQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NutritionalTracker.Database;
+
+namespace NutritionalTracker.Queries
+{
+    public class GetAllProducersQueryHandler : IQueryHandler<GetAllProducersQuery, IReadOnlyList<Producer>>
+    {
+        private readonly INutrionalModel _context;
+
+        public GetAllProducersQueryHandler(INutrionalModel context) {
+            _context = context;
+        }
+
+        public IReadOnlyList<Producer> Handle(GetAllProducersQuery query) {
+            return _context.Producers.OrderBy(m => m.Name).ToList();
+        }
+    }
+}
