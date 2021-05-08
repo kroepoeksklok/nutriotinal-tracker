6be76fabd6aad811ccc4299a804819e6aaaa70df
Robbert <robbert.ouwehand@gmail.com>
2021-05-08 12:33:48 +0200

    Add query and handler for getting products

---
+++ NutritionalTracker/Queries/GetProductsForDailyLogHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using NutritionalTracker.Database;
+
+namespace NutritionalTracker.Queries
+{
+    public class GetProductsForDailyLogHandler : IQueryHandler<GetProductsForDailyLogQuery, IReadOnlyList<Product>>
+    {
+        private readonly INutrionalModel _context;
+
+        public GetProductsForDailyLogHandler(INutrionalModel context) {
+            _context = context;
+        }
+
+        public IReadOnlyList<Product> Handle(GetProductsForDailyLogQuery query) {
+            return _context.Products
+                .Where(product => !product.IsIngredientOnly)
+                .Include(product => product.Producer)
+                .Include(product => product.Unit)
+                .OrderByDescending(product => product.IsFavourite)
+                .ThenBy(product => product.Producer.Name)
+                .ThenBy(product => product.Name)
+                .ToList();
+        }
+    }
+}
---
+++ NutritionalTracker/Queries/GetProductsForDailyLogQuery.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+using NutritionalTracker.Database;
+
+namespace NutritionalTracker.Queries
+{
+    public class GetProductsForDailyLogQuery : IQuery<IReadOnlyList<Product>> { }
+}
