38dedeb7b2bb207da5a1af84a7b4f2fd43354909
Robbert <robbert.ouwehand@gmail.com>
2021-05-08 18:57:15 +0200

    Set DataContext and Text binding property

--- NutritionalTracker/UserControls/AddNewProducer.xaml.cs
+++ NutritionalTracker/UserControls/AddNewProducer.xaml.cs
@@ -1,28 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using NutritionalTracker.ViewModels;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace NutritionalTracker.UserControls
 {
     /// <summary>
     /// Interaction logic for AddNewProducer.xaml
     /// </summary>
-    public partial class AddNewProducer : UserControl
+    public partial class AddNewProducer
     {
         public AddNewProducer()
         {
             InitializeComponent();
+            DataContext = ViewModelLocator.AddProducerViewModel;
         }
     }
 }
