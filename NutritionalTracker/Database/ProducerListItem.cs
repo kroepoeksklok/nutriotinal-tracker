7d605e6951fa2d647419207974754a2e96887ead
Robbert <robbert.ouwehand@gmail.com>
2021-05-08 14:18:01 +0200

    Add the ProducerListItem class

---
+++ NutritionalTracker/Database/ProducerListItem.cs
@@ -0,0 +1,9 @@
+﻿namespace NutritionalTracker.Database
+{
+    public sealed class ProducerListItem
+    {
+        public int ProducerId { get; set; }
+        public string Name { get; set; }
+        public bool CanBeDeleted { get; set; }
+    }
+}
