e333973f0e97120909c5f3c3e2f0f70b7866f099
Robbert <robbert.ouwehand@gmail.com>
2021-05-08 14:48:15 +0200

    Add the ProducersViewModel to the locator

--- NutritionalTracker/ViewModels/ViewModelLocator.cs
+++ NutritionalTracker/ViewModels/ViewModelLocator.cs
@@ -1,5 +1,6 @@
 ﻿namespace NutritionalTracker.ViewModels {
     public static class ViewModelLocator {
         public static MainWindowViewModel MainWindowViewModel => SimpleInjectorContainer.Get<MainWindowViewModel>();
+        public static ProducersViewModel ProducersViewModel => SimpleInjectorContainer.Get<ProducersViewModel>();
     }
 }
